c31fb32d7866c80b723700b5e07dbb45ef6e2a98
Hasan Rahman <hasanrahman33605@gmail.com>
2024-02-14 14:34:36 +0600

    component

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,9 @@ import Footer from "./Footer/Footer"
 import Container from "./container/container"
 import LogoutBtn from "./Header/LogoutBtn"
 import Logo from "./Logo"
+import Input from "./Input"
+import Button from "./Buttton"
 
 
 
-export {Header,Footer,Container,LogoutBtn,Logo}
+export {Header,Footer,Container,LogoutBtn,Logo,Button,Input}
